0c6ca9afbf591c4db40c09a9c49d7d20c2ba23c2
Irakli <ir.mchedlishvili@gmail.com>
2016-04-03 13:28:15 +0400

    script modified - header added in it mannualy

--- scripts/process.py
+++ scripts/process.py
@@ -28,10 +28,73 @@ def process():
 	
 	'''		
 	
-	with open('header.csv', 'r') as csv_data:
-		csv_reader = csv.reader(csv_data)
-		for line in csv_reader:
-			header = line
+	header = [
+		'Date',
+		'All Commodity Price Index',
+		'Non-Fuel Price Index',
+		'Food and Beverage Price Index',
+		'Food Price Index',
+		'Beverage Price Index',
+		'Industrial Inputs Price Index',
+		'Agricultural Raw Materials Index',
+		'Metals Price Index',
+		'Fuel (Energy) Index',
+		'Crude Oil (petroleum)',
+		'Aluminum',
+		'Bananas',
+		'Barley',
+		'Beef',
+		'Coal',
+		'Cocoa beans',
+		'Coffee (Other Mild Arabicas)',
+		'Coffee (Robusta)',
+		'Rapeseed oil',
+		'Copper',
+		'Cotton',
+		'Fishmeal',
+		'Groundnuts (peanuts)',
+		'Hides',
+		'China import Iron Ore Fines 62% FE spot (CFR Tianjin port)',
+		'Lamb',
+		'Lead',
+		'Soft Logs',
+		'Hard Logs',
+		'Maize (corn)',
+		'Natural Gas (Russian Natural Gas border price in Germany)',
+		'Natural Gas (Indonesian Liquefied Natural Gas in Japan)',
+		'Natural Gas (Spot price at the Henry Hub terminal in Louisiana)',
+		'Nickel',
+		'Crude Oil (petroleum-simple average of three spot prices)',
+		'Crude Oil (petroleum - Dated Brent light blend)',
+		'Oil Dubai',
+		'Crude Oil (petroleum - West Texas Intermediate 40 API)',
+		'Olive Oil',
+		'Oranges',
+		'Palm oil',
+		'Swine (pork)',
+		'Poultry (chicken)',
+		'Rice',
+		'Rubber',
+		'Fish (salmon)',
+		'Hard Sawnwood',
+		'Soft Sawnwood',
+		'Shrimp',
+		'Soybean Meal',
+		'Soybean Oil',
+		'Soybeans',
+		'Sugar (European import price)',
+		'Sugar (Free Market)',
+		'Sugar (U.S. import price)',
+		'Sunflower oil',
+		'Tea',
+		'Tin',
+		'Uranium',
+		'Wheat',
+		'Wool (coarse)',
+		'Wool (fine)',
+		'Zinc'
+	]
+	
 	with xlrd.open_workbook('archive/external-data.xls') as xls_data:
 		sheet= xls_data.sheet_by_index(0)
 		col_num = sheet.ncols
