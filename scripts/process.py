3c53ecad56104cdd8c6a801d8a7838e68cc75648
Yedige Ashmet <55581804+gradedSystem@users.noreply.github.com>
2023-09-20 07:46:40 +0200

    Creating pull requests for the commodity-prices (#6)
    * Create update.yml
    
    * Update update.yml
    
    * Update update.yml
    
    * Update update.yml
    
    * Update update.yml
    
    * Change cron schedule time
    
    * [ex][s] fixed github CI/CD - #1
    
    * [ex][s] add requirements & fixed workflow - #2
    
    * [ex][s] added workflow dispatch for manual trigger - 3
    
    * [ex][s] added workflow dispatch for manual trigger - 4
    
    * [ex][m] small changes in process.py - 5
    
    * [ex][m] small changes in process.py - 6
    
    * [ex][m] update requirements - 7
    
    * [ex][s] update yaml with github token - 8
    
    * Auto-update of the data package
    
    * [ex][m] added update data - 9
    
    * Auto-update of the data package
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- scripts/process.py
+++ scripts/process.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf8 -*-
 
 import os
-import urllib
+from urllib import request
 import datetime
 import csv
 import xlrd
@@ -21,7 +21,7 @@ def retrieve(source):
 	'''Downloades xls data to archive directory
 	
 	'''
-	urllib.urlretrieve(source,'archive/external-data.xls')
+	request.urlretrieve(source,'archive/external-data.xls')
 
 def process():
 	'''Gets the data from xls file and puts them in seperete csv files for each comoditie
@@ -96,7 +96,7 @@ def process():
 	]
 	
 	with xlrd.open_workbook('archive/external-data.xls') as xls_data:
-		sheet= xls_data.sheet_by_index(0)
+		sheet = xls_data.sheet_by_index(0)
 		col_num = sheet.ncols
 		row_num = sheet.nrows
 		with open('data/commodity-prices.csv', 'w') as csv_file:
@@ -104,7 +104,7 @@ def process():
 			csvwriter.writerow(header)
 			for row in range(4, row_num):
 				csv_row = []
-				for col in range(col_num):
+				for col in range(col_num):		
 					if not col:				
 						date = datetime.date(int(sheet.cell_value(row, col).split('M')[0]), int(sheet.cell_value(row, col).split('M')[1]), 1)		
 						csv_row.append(date)
